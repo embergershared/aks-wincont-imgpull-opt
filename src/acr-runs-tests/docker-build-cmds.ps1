ded3991437f03eef2c944649f2e940495a9f94ee
Emmanuel <bergerat62@gmail.com>
2025-10-17 17:51:29 -0400

    Started ACR pull duration test runs

---
+++ src/acr-runs-tests/docker-build-cmds.ps1
@@ -0,0 +1,14 @@
+#Build the large image
+$sourceImage = "run48-10gb-ltsc2019:latest"
+docker build -f dockerfile-10G-win2019 -t $sourceImage .
+docker images $sourceImage
+
+docker image inspect $sourceImage --format='{{.Size}}'
+
+#Tag the image for ACR
+$acrName = "acrusw3391575s4halwincont"
+$targetImage = "hal-dwp/run48-10gb-ltsc2019:latest"
+
+docker tag $sourceImage "$acrName.azurecr.io/$targetImage"
+#Push the image to ACR
+docker push "$acrName.azurecr.io/$targetImage"
