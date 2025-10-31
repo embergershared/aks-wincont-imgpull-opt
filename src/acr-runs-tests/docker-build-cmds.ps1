56572dfce2b4131dfbd261d9f02d814e5b938741
Emmanuel <bergerat62@gmail.com>
2025-10-31 19:00:55 -0400

    Switched to a large image that can't be compressed.

--- src/acr-runs-tests/docker-build-cmds.ps1
+++ src/acr-runs-tests/docker-build-cmds.ps1
@@ -1,13 +1,13 @@
 #Build the large image
-$sourceImage = "run48-10gb-ltsc2019:latest"
-docker build -f dockerfile-10G-win2019 -t $sourceImage .
+$sourceImage = "run48-winiso-ltsc2019:latest"
+docker build -f dockerfile-10G-random-win2019 -t $sourceImage .
 docker images $sourceImage
 
 docker image inspect $sourceImage --format='{{.Size}}'
 
 #Tag the image for ACR
 $acrName = "acrusw3391575s4halwincont"
-$targetImage = "hal-dwp/run48-10gb-ltsc2019:latest"
+$targetImage = "hal-dwp/run48-winiso-ltsc2019:latest"
 
 docker tag $sourceImage "$acrName.azurecr.io/$targetImage"
 #Push the image to ACR
