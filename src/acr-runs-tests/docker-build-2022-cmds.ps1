e587ef6ab34a97e822262261098ae56665fdd202
Emmanuel <bergerat62@gmail.com>
2025-11-13 21:54:51 -0500

    Testing ACR Premium on win2022 base

---
+++ src/acr-runs-tests/docker-build-2022-cmds.ps1
@@ -0,0 +1,14 @@
+#Build the large image
+$sourceImage = "run48-winiso-ltsc2022:latest"
+docker build -f dockerfile-10G-random-win2022 -t $sourceImage .
+docker images $sourceImage
+
+docker image inspect $sourceImage --format='{{.Size}}'
+
+#Tag the image for ACR
+$acrName = "acrusw3391575s4halwincont"
+$targetImage = "hal-dwp/run48-winiso-ltsc2022:latest"
+
+docker tag $sourceImage "$acrName.azurecr.io/$targetImage"
+#Push the image to ACR
+docker push "$acrName.azurecr.io/$targetImage"
